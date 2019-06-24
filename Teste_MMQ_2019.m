ead37405ad46a1aa4ce332a69a1da4eaef2ce764
Gabriel Negrelli <gabriel.jose.gomes@usp.br>
2019-06-24 16:47:33 +0100

    Add comments to main program

--- Teste_MMQ_2019.m
+++ Teste_MMQ_2019.m
@@ -1,8 +1,13 @@
+clear all
+close all
+clc
+
 n = 100;
 m = 2;
 x = linspace(0.0,2*pi,n);
 f = sin(x);
 
+% Construcao da matriz A
 for i = 1:m+1
     for j = 1:n
         A(j,i) = x(j)^(i-1);
@@ -11,13 +16,16 @@ end
 
 for k = 1:n
     
+    % Vetor de pesos
     for j = 1:n
         w(j) = 1.0/((x(j) - x(k))^2+0.00001);
         w(j) = 1.0;
     end
 
+    % Calculo dos alfas
     alfa = MMQ_2019(A,w,f');
 
+    
     p(k) = 0.0;
     for i = 1:m+1
         p(k) = p(k) + alfa(i)*x(k)^(i-1);
@@ -27,5 +35,5 @@ end
 
 hold on
 plot(x,f,'b');
-plot(x,p,'r');
+plot(x,p,'r.');
 hold off
